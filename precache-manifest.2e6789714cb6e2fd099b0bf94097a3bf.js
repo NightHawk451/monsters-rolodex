4f61fe8720007639b679e83721515c4cc16324dc
Micah Pearce <micah.r.pearce@gmail.com>
2020-05-23 17:35:05 -0500

    Updates

---
+++ precache-manifest.2e6789714cb6e2fd099b0bf94097a3bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c88bbe0033bc18a7dd1b31e7a29e513",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d0f91526a745dd0cd0a6",
+    "url": "/monsters-rolodex/static/css/main.84d28cc4.chunk.css"
+  },
+  {
+    "revision": "0105f5fa7a131123a102",
+    "url": "/monsters-rolodex/static/js/2.8a77c072.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.8a77c072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0f91526a745dd0cd0a6",
+    "url": "/monsters-rolodex/static/js/main.382d2528.chunk.js"
+  },
+  {
+    "revision": "bc9b3e30e5d8354d0e21",
+    "url": "/monsters-rolodex/static/js/runtime-main.bdaefb98.js"
+  }
+]);
